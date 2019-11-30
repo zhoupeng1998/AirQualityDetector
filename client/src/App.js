7172a3a42c14ba55c1f099d6bcfa999685bba5dc
Alexander <songhenz@uci.edu>
2019-11-30 13:40:24 -0800

    add pages directory to clinet

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Switch, Link, Redirect} from "react-rout
 
 import MainPage from './pages/index';
 import HistoryChart from "./pages/history";
+import RawData from "./pages/raw";
 
 class App extends Component {
 
@@ -12,6 +13,7 @@ class App extends Component {
                 <Switch>
                     <Route exact path='/' component={MainPage} />
                     <Route exact path='/history' component={HistoryChart} />
+                    <Route exact path='/raw' component={RawData} />
                 </Switch>
             </Router>
         );
